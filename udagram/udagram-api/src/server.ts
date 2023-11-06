a6fe00cc5b67f34dce129dfeed0adc30ee00537c
Toan Nguyen <nguyenvantoan263@gmail.com>
2023-11-06 23:53:20 +0800

    retry after testing on local

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -11,6 +11,13 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 (async () => {
   dotenv.config();
 
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully...");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+
   await sequelize.addModels(V0_FEED_MODELS);
   await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
@@ -18,11 +25,31 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  const port = process.env.PORT || 8080;
+  const port = 8080;
 
   app.use(bodyParser.json());
 
-  app.use(cors());
+  // app.use(cors());
+  // We set the CORS origin to * so that we don't need to
+  // worry about the complexities of CORS.
+  app.use(
+    cors({
+      allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "X-Access-Token",
+        "Authorization",
+        "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Methods",
+      ],
+      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+      preflightContinue: true,
+      origin: "*",
+    })
+  );
 
   app.use("/api/v0/", IndexRouter);
 
@@ -33,7 +60,8 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Start the Server
   app.listen(port, () => {
-    console.log(`server running ${process.env.URL}`);
+    console.log(`Backend server is listening on port ${port}....`);
+    console.log(`Frontent server running ${process.env.URL}`);
     console.log(`press CTRL+C to stop server`);
   });
 })();
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: "Udagram",
   apiHost:
-    "http://udagram-api-env.eba-rjbpi4w2.us-east-1.elasticbeanstalk.com/api/v0",
+    "http://udagram-api-dev.eba-vnfarc5x.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
